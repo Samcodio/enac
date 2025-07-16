0d7f2ef98ccab0e818f1c9f45fc265da933ed6ab
Samcodio <nosikesamuel1@gmail.com>
2025-07-16 14:53:34 -0700

    static

--- enac/settings.py
+++ enac/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8e2_#uqa^+*nd1kx^#fpmk3_0@@u8bw0)k*nyse7ff+loukee+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -89,19 +89,19 @@ WSGI_APPLICATION = 'enac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-database_url = "postgresql://samuel_mnny_user:wqGKBp8RSH95ID4LmP2kD5p2VWpKkeMc@dpg-d1lf17vdiees73fjseng-a.oregon-postgres.render.com/samuel_mnny"
-
 DATABASES = {
-    "default": dj_database_url.parse(database_url)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# database_url = "postgresql://samuel_mnny_user:wqGKBp8RSH95ID4LmP2kD5p2VWpKkeMc@dpg-d1lf17vdiees73fjseng-a.oregon-postgres.render.com/samuel_mnny"
+#
+# DATABASES = {
+#     "default": dj_database_url.parse(database_url)
+# }
 
 
 # Password validation
@@ -137,12 +137,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+AWS_ACCESS_KEY_ID = 'AKIA2UC27BOKBK57GZ6V'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'noxmart'
+AWS_S3_REGION_NAME = 'eu-north-1'
+
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
